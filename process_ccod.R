6b6570a8be30600c9905141414a71c2fcebe69e3
Adam Watkins <adam@stupidpupil.co.uk>
2021-06-18 17:43:49 +0100

    Capitalise UK and NHS in land lowner names

--- process_ccod.R
+++ process_ccod.R
@@ -2,8 +2,6 @@ ccod <- read_csv("data-raw/CCOD_FULL_2021_06.zip")
 ccod <- ccod %>% filter(Postcode %>% str_detect('^CF'))
 
 
-
-
 ccod <- ccod %>% add_fields_based_on_postcode(fields=c("OA11Code", "LocalAuthorityName")) %>%
   select(OA11Code, LocalAuthorityName, `Proprietor Name (1)`) %>%
   rename(Owner =  `Proprietor Name (1)`) %>%
@@ -19,7 +17,9 @@ normalize_owner_name <- function(owner_name){
     str_replace_all("\\s+", " ") %>% str_trim() %>%
     str_to_title() %>%
     str_replace_all("The County Council Of The City & County Of Cardiff", "Cardiff Council") %>%
-    str_replace_all("The Council Of The City Of Cardiff", "Cardiff Council")
+    str_replace_all("The Council Of The City Of Cardiff", "Cardiff Council") %>%
+    str_replace_all("`\\b(Uk)\\b", "UK") %>%
+    str_replace_all("`\\b(Nhs)\\b", "NHS")
 }
 
 
